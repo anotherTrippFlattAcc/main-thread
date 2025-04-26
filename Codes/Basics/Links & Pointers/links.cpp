5b74e003e369505d3fe9b0ae4443316222a26eac
TrippFlatt <trippflatt@gmail.com>
2025-04-26 16:10:40 +0500

    Added links.cpp file

---
+++ Codes/Basics/Links & Pointers/links.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    cout << "Value of a: " << a << endl;
+    cout << "Address of a: " << &a << endl;
+
+    int b = 10;
+    cout << "Value of b: " << b << endl;
+    cout << "Address of b: " << &b << endl;
+}
